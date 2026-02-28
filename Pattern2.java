7e529b03152ed8e8719f62aeb5e67ec8e2469cc4
PULLIGANI ROOPESH <roopeshp771@gmail.com>
2026-02-28 11:34:56 +0530

    Add files via upload

---
+++ Pattern2.java
@@ -0,0 +1,15 @@
+public class Pattern2 {
+    public static void main(String[] args)
+    {
+        int i,j;
+        int n=5;
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("a");
+            }
+            System.out.println(" ");
+        }
+    }
+}   
